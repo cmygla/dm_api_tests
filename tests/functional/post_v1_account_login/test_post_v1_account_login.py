4673e6d62e24d52eb6d1b1e27e4c8558fd240349
Ek.Kravchenko <EkViKravchenko@rosatom.ru>
2025-01-12 17:54:28 +0400

    Fix filename

--- tests/functional/post_v1_account_login/test_post_v1_account_login.py
+++ tests/functional/post_v1_account_login/test_post_v1_account_login.py
@@ -1,4 +1,4 @@
 def test_post_v1_account_login_not_activated_user(login_api, created_user):
     # авторизация
     response = login_api.post_v1_account_login(login=created_user["login"], password=created_user["password"])
-    assert response.status_code == 403, "Не активированный пользователь авторизовался"
+    assert response.status_code == 403, "Неактивированный пользователь авторизовался"

473fb0eaf8e2bdab5eaab21201d4ba16b2e9ccbb
Ek.Kravchenko <EkViKravchenko@rosatom.ru>
2025-01-15 23:02:17 +0400

    Added fasade of fasades removed fixtures

--- tests/functional/put_v1_account_token/test_put_v1_account_token.py
+++ tests/functional/put_v1_account_token/test_put_v1_account_token.py
@@ -1,8 +1,10 @@
 import uuid
 
+from conftest import account_helper
 
-def test_put_v1_account_token_not_valid(account_api):
+
+def test_put_v1_account_token_not_valid(account_helper):
     # активация  с невалидным токеном
     token = str(uuid.uuid4())
-    response = account_api.put_v1_account_token(token=token)
+    response = account_helper.dm_api_account.account_api.put_v1_account_token(token=token)
     assert response.status_code == 410, "Пользователь был активирован с невалидным токеном"
